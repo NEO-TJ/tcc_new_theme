38d552ff26131f030ee4118cbc6db5e6d6436088
NEO-TJ <koravit_ton@hotmail.com>
2017-12-06 00:35:22 +0700

    fix bug userAuth_m

--- application/models/userAuthentication_m.php
+++ application/models/userAuthentication_m.php
@@ -24,14 +24,14 @@ class UserAuthentication_m extends CI_Model {
 // Public function.
     // ------------------------------------------------- Validate --------------------------------------
     public function Validate() {
-		$this->load->model('dataclass/user_d');
+		$this->load->model('dataclass/users_d');
 		$this->load->model('db_m');
 
-        $this->db_m->tableName = $this->user_d->tableName;
+        $this->db_m->tableName = $this->users_d->tableName;
         $arrWhere = [
-					$this->user_d->colUserId   => $this->userId,
-					$this->user_d->colPassword => $this->password,
-					$this->user_d->colStatus   => '1'
+					$this->users_d->colUserId   => $this->userId,
+					$this->users_d->colPassword => $this->password,
+					$this->users_d->colStatus   => '1'
         ];
 		$result = $this->db_m->GetRowWhere($arrWhere);
 
